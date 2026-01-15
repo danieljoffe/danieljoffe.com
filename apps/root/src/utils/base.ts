78a84df226a69aebb38410dde2d08120d548a6aa
Daniel Joffe <9543755+dannyk08@users.noreply.github.com>
2026-01-15 12:25:33 -0800

    feat: add centralized navigation links utility
    - Introduced a new utility file for navigation links, defining constants for Home, About, Projects, and Experience links.
    - This change centralizes link definitions, improving maintainability and consistency across the application.
    
    These modifications aim to streamline navigation management and enhance code organization.

---
+++ apps/root/src/utils/base.ts
@@ -0,0 +1,16 @@
+import { NavLinkI } from '@/types/base';
+
+export const HOME_LINK: NavLinkI = { href: '/', label: 'Home' };
+export const ABOUT_LINK: NavLinkI = { href: '/about', label: 'About' };
+export const PROJECTS_LINK: NavLinkI = { href: '/projects', label: 'Projects' };
+export const EXPERIENCE_LINK: NavLinkI = {
+  href: '/experience',
+  label: 'Experience',
+};
+
+export const NAV_LINKS: NavLinkI[] = [
+  HOME_LINK,
+  ABOUT_LINK,
+  EXPERIENCE_LINK,
+  PROJECTS_LINK,
+];
