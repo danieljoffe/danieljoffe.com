9439e901ca643d03c0b5d531a1339d50400bd4b2
Daniel Joffe <9543755+dannyk08@users.noreply.github.com>
2026-02-04 12:22:52 -0800

    update types

--- apps/root/src/data/projectThumbnails.ts
+++ apps/root/src/data/projectThumbnails.ts
@@ -1,5 +1,5 @@
 import { UNSPLASH_URL } from '@/utils/constants';
-import { PostThumbnailI } from '@/types/post.types';
+import { PostThumbnailI } from '@/types/postTypes';
 import { AllowedProjectSlugs } from '@/types/base';
 import { projectSlugs } from '@/data/project';
 import { PROJECTS_LINK } from '@/utils/base';
